56c517a99219a15e16a319730690bff23421366c
Aleks <aleks@streetline.com>
2015-08-16 22:03:15 -0700

    Adding 'mario.c'

---
+++ CS/mario.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    int n = 0;
+    do
+    {
+        printf("Please enter number between 0 and 23: ");
+        n = GetInt();
+        
+    }
+    while(n < 0 || n > 23);
+    
+    
+    if (n == 1)
+    {
+        printf("##\n");
+    }
+    else if ( n > 1)
+    {
+        int i;
+        for (i = 0; i < n; i++)
+        {
+            int s;
+            for(s = i; n > (s + 1); s++)
+            {
+                printf(" ");
+            }
+            
+            int h;
+            for(h = i; h + 2 > 0; h--)
+            {
+                printf("#");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
