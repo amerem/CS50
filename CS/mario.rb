ff0be7ff5a77034d3c40411d6d3868bf62ebe596
Aleksandr Mereminskiy <aleksandrmereminskiy@Aleksandrs-MacBook-Pro.local>
2022-11-03 17:19:05 -0700

    PR has code improvements and fixes rubocop messages/errors

--- CS/mario.rb
+++ CS/mario.rb
@@ -1,11 +1,10 @@
-#/usr/local/bin/ruby
+# frozen_string_literal:true
 
-
-print "Please enter hight: "
+puts 'Please enter hight: '
 i = gets.chomp.to_i
 max = 23
-while i < 1 or i > max
-  print "Hight should be between 1 and #{max}: "
+while i < 1 || i > max
+  puts "Hight should be between 1 and #{max}: "
   i = gets.chomp.to_i
 end
 puts "Thank you! You entered #{i}."
@@ -13,4 +12,4 @@ puts "Thank you! You entered #{i}."
 spaces = max - i
 hashes = max
 
-spaces.upto(hashes) {|x| puts " " * (max - x) + "#" * x }
+spaces.upto(hashes) { |x| puts ' ' * (max - x) + '#' * x }
